63019729599d1f24aa58a567ca3b1bf53fbf3175
lleonardofarias <Mantzzcanal@gmail.com>
2023-08-18 14:35:41 -0300

    Config-att

--- climapp/Main-code.py
+++ climapp/Main-code.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 import requests
+from configparser import ConfigParser
 
-def get_weather(api_key, city):
-    base_url = ""
+config = ConfigParser()
+config.read("config.ini")
+
+def get_weather(api_key, city, units):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": city,
         "appid": api_key,
-        "units": "metric" 
+        "units": units  # Use the units specified by the user
     }
 
     response = requests.get(base_url, params=params)
@@ -15,18 +19,27 @@ def get_weather(api_key, city):
     return data
 
 def main():
-    api_key = "" #your weather API.
+    api_key = config.get('Weather', 'appid')
     city = input("Name the city: ")
+    units = input("Select temperature units (C/F/K): ").lower()
+    if units == "c":
+        units = "metric"
+    elif units == "f":
+        units = "imperial"
+    elif units == "k":
+        units = "standard"
+    else:
+        print("Invalid units. Using default units.")
+        units = config.get('Weather', 'units')
+
     local_dt = datetime.now()
     hour = local_dt.strftime("%H:%M")
 
-
-    
-    weather_data = get_weather(api_key, city)
+    weather_data = get_weather(api_key, city, units)  # Provide the units argument here
     if "main" in weather_data:
         temperature = weather_data["main"]["temp"]
         description = weather_data["weather"][0]["description"]
-        print(f"temperature in {city}: {temperature:.2f}°C at {hour}")  
+        print(f"Temperature in {city}: {temperature:.2f} in the metric that you choose, at {hour}")
         print(f"Condition: {description}")
     else:
         print("Unable to get weather data")
@@ -41,4 +54,4 @@ def repeat():
 
 if __name__ == "__main__":
     main()
-    repeat()       
+    repeat()
---
+++ readme.md
@@ -0,0 +1,3 @@
+Mini program in python that shows the weather and time of a place. Maybe I can try to use django in the project in the future.
+
+For the program to work correctly, just put your weather API in the "config.ini" file, and have fun!
